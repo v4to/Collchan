18492d9ec96f76631b12a3446a62fe0db144db47
t1monchik <andrew.egorcev@gmail.com>
2020-05-21 18:58:05 +0300

    Initial Commit

---
+++ Makaba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Makaba
+//
+//  Created by Andrii Yehortsev on 21.05.2020.
+//  Copyright © 2020 Andrii Yehortsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
