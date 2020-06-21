aea3427b010dc9d4a5c9b139ef84e3544f4499a5
v4to <andrew.egorcev@gmail.com>
2020-06-21 18:05:42 +0300

    Remade network layer

---
+++ Makaba/Services/NetworkRequests.swift
@@ -0,0 +1,86 @@
+//
+//  NetworkRequests.swift
+//  Thunder
+//
+//  Created by Andrii Yehortsev on 21.06.2020.
+//  Copyright © 2020 Andrii Yehortsev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+protocol NetworkRequest: AnyObject {
+    associatedtype ModelType
+    func decode(_ data: Data) -> ModelType?
+    func load(withCompletion completion: @escaping (ModelType?) -> Void)
+}
+
+extension NetworkRequest {
+    func load(_ url: URL, withCompletion completion: @escaping (ModelType?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data: Data?, response: URLResponse?, error: Error?) in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            let res = self?.decode(data)
+            DispatchQueue.main.async {
+                completion(res)
+            }
+        }
+        task.resume()
+    }
+}
+
+class ImageRequest {
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+}
+
+extension ImageRequest: NetworkRequest {
+    func decode(_ data: Data) -> UIImage? {
+        return UIImage(data: data)
+    }
+    
+    func load(withCompletion completion: @escaping (UIImage?) -> Void) {
+        load(url, withCompletion: completion)
+    }
+}
+
+class APIRequest<Resource: APIResource> {
+    let resource: Resource
+    
+    init(resource: Resource) {
+        self.resource = resource
+    }
+}
+
+extension APIRequest: NetworkRequest {
+    func decode(_ data: Data) -> Resource.ModelType? {
+        let wrapper = try? JSONDecoder().decode(Resource.ModelType.self, from: data)
+        return wrapper!
+    }
+    
+    func load(withCompletion completion: @escaping (Resource.ModelType?) -> Void) {
+        load(resource.url, withCompletion: completion)
+    }
+}
+
+protocol APIResource {
+    associatedtype ModelType: Decodable
+    var methodPath: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+extension APIResource {
+    var url: URL {
+        var components = URLComponents(string: BaseUrls.dvach)!
+        components.path = methodPath
+        components.queryItems = queryItems
+        return components.url!
+    }
+}
