180b7d1c4ea84a06c0acc701a0c04fd7d00c4de6
v4to <andrew.egorcev@gmail.com>
2021-06-16 21:02:42 +0300

    Move UIImage extension to separate file

---
+++ Kapchan/Extensions/UIImage.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkRequests.swift
+//  Thunder
+//
+//  Created by Andrii Yehortsev on 21.06.2020.
+//  Copyright © 2020 Andrii Yehortsev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+  func resized(to newSize: CGSize) -> UIImage? {
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+    defer { UIGraphicsEndImageContext() }
+
+    draw(in: CGRect(origin: .zero, size: newSize))
+    return UIGraphicsGetImageFromCurrentImageContext()
+  }
+}
